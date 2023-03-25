5e2191c2233142734453e4fa91948f1f7095f2e0
mathantr <128468590+mathantr@users.noreply.github.com>
2023-03-25 18:23:29 +0530

    Add files via upload

---
+++ lib/colors/const.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/animation.dart';
+
+final black = Color.fromRGBO(23, 28, 40, 1);
+final liteGreen = Color.fromRGBO(151, 233, 164, 1);
+final white = Color.fromRGBO(251, 251, 251, 1);
+final blue = Color.fromRGBO(50, 64, 134, 1);
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import '../screens/home_screeen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        fontFamily: 'PT Sans Caption',
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
