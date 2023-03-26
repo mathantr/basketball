b9ab339b558186e5e43f31c7c56d58fe21969949
mathantr <128468590+mathantr@users.noreply.github.com>
2023-03-26 21:35:22 +0530

    Add files via upload

---
+++ lib/model/upcoming.dart
@@ -0,0 +1,7 @@
+class UpcomingCat {
+  final String iUrl;
+  final String iColor;
+  final String iBgColor;
+  UpcomingCat(
+      {required this.iUrl, required this.iColor, required this.iBgColor});
+}
