9ac0d7b60340c1149bd607f9748b270fcea30963
mathantr <128468590+mathantr@users.noreply.github.com>
2023-03-28 18:00:32 +0530

    Delete lib directory

--- lib/model/upcoming.dart
+++
@@ -1,7 +0,0 @@
-class UpcomingCat {
-  final String iUrl;
-  final String iColor;
-  final String iBgColor;
-  UpcomingCat(
-      {required this.iUrl, required this.iColor, required this.iBgColor});
-}
