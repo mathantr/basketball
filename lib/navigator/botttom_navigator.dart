0481c8ab7340bd6df53190c571214037a120ff9c
mathantr <128468590+mathantr@users.noreply.github.com>
2023-03-28 18:04:11 +0530

    Add files via upload

---
+++ lib/navigator/botttom_navigator.dart
@@ -0,0 +1,68 @@
+import 'package:basketball/colors/const.dart';
+import 'package:basketball/screens/home_screeen.dart';
+import 'package:basketball/screens/profile_screen.dart';
+import 'package:flutter/material.dart';
+
+class MyBottomNavigationBar extends StatefulWidget {
+  const MyBottomNavigationBar({Key? key}) : super(key: key);
+
+  @override
+  _MyBottomNavigationBarState createState() => _MyBottomNavigationBarState();
+}
+
+class _MyBottomNavigationBarState extends State<MyBottomNavigationBar> {
+  int _selectedIndex = 0;
+
+  static const List<Widget> _widgetOptions = <Widget>[
+    HomeScreen(),
+    Text('Index 1: Business'),
+    Text('Index 2: School'),
+    Text('Index 3: Settings'),
+    ProfileScreen(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: black,
+        selectedItemColor: liteGreen,
+        unselectedItemColor: Colors.white.withOpacity(0.4),
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.sports_basketball),
+            label: 'Sport',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.video_settings),
+            label: 'Live',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.messenger_rounded),
+            label: 'Chat',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+}
---
+++ lib/screens/home_screeen.dart
@@ -0,0 +1,54 @@
+import '../colors/const.dart';
+import '../widgets/home/appbar.dart';
+import '../widgets/home/free_card.dart';
+import '../widgets/home/uefa.dart';
+import '../widgets/home/upcoming.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return Scaffold(
+      backgroundColor: black,
+      body: SafeArea(
+        child: SizedBox(
+          height: size.height,
+          width: size.width,
+          child: SingleChildScrollView(
+            physics:
+                BouncingScrollPhysics(parent: AlwaysScrollableScrollPhysics()),
+            child: Stack(
+              children: [
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Appbar(),
+                      SizedBox(
+                        height: 7,
+                      ),
+                      FreeCard(),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      UpComing(),
+                      SizedBox(
+                        height: 25,
+                      ),
+                      Uefa(),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
